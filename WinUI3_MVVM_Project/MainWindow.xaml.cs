425d19f952be97f26b2987d69850eaa5cf00243d
LeBaThinh <lebathinh203@gmail.com>
2025-06-12 19:30:09 +0700

    thêm full chức năng

--- WinUI3_MVVM_Project/MainWindow.xaml.cs
+++ WinUI3_MVVM_Project/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ public sealed partial class MainWindow : Window
         // Thiết lập tiêu đề cho cửa sổ
         Title = "WinUI 3 MVVM Project";
         //Thêm control
+        //Thêm full chức năng
 
         // Kích thước cửa sổ ban đầu (tùy chọn)
         // AppWindow.Resize(new Windows.Graphics.SizeInt32(1024, 768));
